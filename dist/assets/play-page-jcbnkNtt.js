614c5d3aa2b1abc44d1016874bec7ee4ca4b87a1
burundu4ok2000 <burundu4ok2000@gmail.com>
2025-12-25 17:08:00 +0300

    build

--- dist/assets/play-page-jcbnkNtt.js
+++
@@ -1,1 +0,0 @@
-import"./style-PHdBYtyz.js";const e=document.querySelector(".site-header");e&&window.addEventListener("scroll",()=>{window.scrollY>20?e.classList.add("scrolled"):e.classList.remove("scrolled")});

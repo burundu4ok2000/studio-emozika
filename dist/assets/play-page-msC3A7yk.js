f1eafb706ec247011931ae6cd031aba4dda67dc1
burundu4ok2000 <burundu4ok2000@gmail.com>
2025-12-25 17:15:24 +0300

    build

---
+++ dist/assets/play-page-msC3A7yk.js
@@ -0,0 +1,1 @@
+import"./style-DlH6HokV.js";const e=document.querySelector(".site-header");e&&window.addEventListener("scroll",()=>{window.scrollY>20?e.classList.add("scrolled"):e.classList.remove("scrolled")});

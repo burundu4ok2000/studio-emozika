0cbbb401b6b975fc1dcaf62cf074bbc89e18ce00
burundu4ok2000 <burundu4ok2000@gmail.com>
2025-12-25 17:25:05 +0300

    Updates

--- assets/play-page-Dgd8mWnN.js
+++
@@ -1,1 +0,0 @@
-import"./style-B1UpgiwS.js";const e=document.querySelector(".site-header");e&&window.addEventListener("scroll",()=>{window.scrollY>20?e.classList.add("scrolled"):e.classList.remove("scrolled")});
---
+++ assets/play-page-Dz-U0Hp9.js
@@ -0,0 +1,1 @@
+import"./style-03ehRjOF.js";const e=document.querySelector(".site-header");e&&window.addEventListener("scroll",()=>{window.scrollY>20?e.classList.add("scrolled"):e.classList.remove("scrolled")});
